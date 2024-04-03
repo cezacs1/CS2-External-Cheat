fdba861b8980b0da24b650dd1776825f92174eba
C E Z A#6466 <93098721+cezacs1@users.noreply.github.com>
2024-04-03 15:28:20 +0300

    Add files via upload

---
+++ CG-ExternalSharp/CG-ExternalSharp/Offsets.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_ExternalSharp
+{
+    public class Offsets
+    {
+        public static class client_dll
+        {
+            // https://github.com/a2x/cs2-dumper/blob/main/output/offsets.cs
+            public static ulong dwEntityList = 0x18C1DB8;
+            public static ulong dwLocalPlayerController = 0x1911578;
+            public static ulong dwLocalPlayerPawn = 0x17361E8;
+
+            // https://github.com/a2x/cs2-dumper/blob/archive/client.dll.cs
+            public static ulong m_iHealth = 0x334; // int32_t
+            public static ulong m_flFlashDuration = 0x14CC; // float
+            public static ulong m_fFlags = 0x3D4; // uint32_t
+            public static ulong m_iTeamNum = 0x3CB; // uint8_t
+            public static ulong m_hPawn = 0x604; // CHandle<C_BasePlayerPawn>
+            public static ulong m_flDetectedByEnemySensorTime = 0x1440; // GameTime_t
+
+            public static ulong spotted = 0x1698 + 0xC; // m_entitySpottedState + m_bSpottedByMask
+
+
+            // https://github.com/a2x/cs2-dumper/blob/main/output/buttons.cs
+            public static ulong jump = 0x172F570;
+        }
+
+        public static class engine2_dll
+        {
+            public static ulong dwBuildNumber = 0x514574;
+            public static ulong dwNetworkGameClient_deltaTick = 0x258;
+            public static ulong dwNetworkGameClient = 0x513AC8;
+            public static ulong dwNetworkGameClient_getLocalPlayer = 0xF0;
+            public static ulong dwNetworkGameClient_maxClients = 0x250;
+            public static ulong dwNetworkGameClient_signOnState = 0x240;
+            public static ulong dwWindowHeight = 0x5CCCDC;
+            public static ulong dwWindowWidth = 0x5CCCD8;
+        }
+
+        public static class game_info
+        {
+            public static ulong buildNumber = 0x36B0; // Game build number
+        }
+
+        public static class inputsystem_dll // inputsystem.dll
+        { 
+            public static ulong dwInputSystem = 0x367A0;
+        }
+
+        public static class matchmaking_dll // matchmaking.dll
+        { 
+            public static ulong dwGameTypes = 0x1D21E0;
+            public static ulong dwGameTypes_mapName = 0x1D2300;
+        }
+    }
+}
---
+++ CG-ExternalSharp/CG-ExternalSharp/WinAPI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using static CG_ExternalSharp.Memory;
+
+namespace CG_ExternalSharp
+{
+    public class WinAPI
+    {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int key);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+             Enums.ProcessAccessFlags processAccess,
+             bool bInheritHandle,
+             int processId
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(
+            IntPtr hProcess,
+            ulong lpBaseAddress,
+            [Out] byte[] lpBuffer,
+            int dwSize,
+            out IntPtr lpNumberOfBytesRead
+        );
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(
+             IntPtr hProcess,
+             ulong lpBaseAddress,
+             byte[] lpBuffer,
+             int dwSize,
+             out IntPtr lpNumberOfBytesWritten
+        );
+    }
+}
